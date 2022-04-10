2956c562b58302637a8f5db112b44cbfcfc7896b
sparrowid <102942689+sparrowid@users.noreply.github.com>
2022-04-10 11:05:32 +0530

    Add files via upload

---
+++ Basic Data Types Activity/conditionals.js
@@ -0,0 +1,10 @@
+let age = 100;
+if (age >= 5 && age < 12) {
+    console.log("you must be in elementary school")
+}
+else if(age >= 12 && age <= 18) {
+    console.log("you must be in middle school")
+}
+else {
+    console.log("you must be in college")
+}
